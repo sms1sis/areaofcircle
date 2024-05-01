e001539d0c9c5b469d71ee2269d7f0e6c802aee3
Sadi Mahmud Sajid <153254393+sadimahmud2004@users.noreply.github.com>
2024-05-01 21:27:27 +0600

    ...

--- area-of-a-circle.c
+++ area-of-a-circle.c
@@ -1,16 +1,3 @@
-/*
-Welcome to the world of coding!
-File: [File Name]
-Author: Sadi Mahmud Sajid & Devildon
-
-Description: [Brief description of the file's purpose]
-
-We're thrilled to have you here! Whether you're a seasoned programmer or just starting your journey, this code is a testament to your curiosity and ambition. Let's dive in together and explore the endless possibilities of C programming.
-
-Happy coding!
-Sadi Mahmud Sajid & Devildon
-*/
-
 #include <stdio.h>
 #include <stdarg.h>
 #include <sys/ioctl.h>
