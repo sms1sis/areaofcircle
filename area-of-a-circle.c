1634fcfdfac88e7b13cf3d9ba15ae623820422a1
Sadi Mahmud Sajid <153254393+sms1sis@users.noreply.github.com>
2025-09-20 02:17:12 +0600

    Update area-of-a-circle.c
    Fixed printf_centerd issue and value of r

--- area-of-a-circle.c
+++ area-of-a-circle.c
@@ -5,20 +5,23 @@
 #include <string.h>
 
 void print_centered(const char *fmt, ...) {
-
     struct winsize w;
     ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
     int term_width = w.ws_col;
-    int padding = (term_width - strlen(fmt)) / 2;
+
+    char buffer[256];
+    va_list args;
+    va_start(args, fmt);
+    vsnprintf(buffer, sizeof(buffer), fmt, args);
+    va_end(args);
+
+    int len = strlen(buffer);
+    int padding = (term_width - len) / 2;
 
     for (int i = 0; i < padding; ++i) {
         putchar(' ');
     }
-
-    va_list args;
-    va_start(args, fmt);
-    vprintf(fmt, args);
-    va_end(args);
+    printf("%s", buffer);
 }
 
 int main() {
@@ -38,43 +41,47 @@ int main() {
     print_centered("(devil㉿kali)\n");
     print_centered("Contact Number:01890731620\n");
     print_centered("Gmail:sadimahmudsajid@gmail.com\n");
-    printf("\033[0m");   
+    printf("\033[0m");
     printf("\033[0m");
     printf("\033[0m");
     printf("\n");
-
     print_centered("\033[1;32m        >>>C program to calculate the area of a circle<<<\n");
 
     char choice;
-
     do {
-        printf("\n");
         float pi = 3.1416;
         float r;
         float sum;
-        print_centered("\033[1;33m     Enter the value of r: ");
-        scanf("%f", &r);
+        int input_check;
 
-        while (r < 0) {
-        printf("\n");
-        print_centered("\033[1;31m       Please enter 'r > 0': ");
-            scanf(" %f", &r);
-        }
+        do {
+            printf("\n");
+            print_centered("\033[1;33m     Enter the value of r: ");
+            input_check = scanf("%f", &r);
+            while(getchar() != '\n'); // Clear the input buffer
 
-        sum = pi*r*r;
-        print_centered("\033[1;33m    The area is %f\n      ", sum);
+            if (input_check != 1) {
+                printf("\n");
+                print_centered("\033[1;31m       Please enter only numbers.\n");
+            } else if (r <= 0) {
+                printf("\n");
+                print_centered("\033[1;31m       Please enter 'r > 0': \n");
+            }
+        } while (input_check != 1 || r <= 0);
+
+        sum = pi * r * r;
+        print_centered("\033[1;33m    The area is %f\n", sum);
 
         printf("\n");
         print_centered("\033[1;35m       Do you want to calculate another area? (y/n): ");
         scanf(" %c", &choice);
 
         while (choice != 'y' && choice != 'n') {
-        printf("\n");
-        print_centered("\033[1;31m       Please enter 'y' or 'n': ");
+            printf("\n");
+            print_centered("\033[1;31m       Please enter 'y' or 'n': ");
             scanf(" %c", &choice);
         }
-
-        } while (choice == 'y');
+    } while (choice == 'y');
 
     return 0;
 }
